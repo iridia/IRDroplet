43858c82d6d3116ba93a4e4a3359282c4e684aa8
Evadne Wu <ev@monoceroi.com>
2011-08-30 00:30:31 +0800

    initial.

---
+++ IRDroplet/IRDroplet.h
@@ -0,0 +1,11 @@
+//
+//  IRDroplet.h
+//  IRDroplet
+//
+//  Created by Evadne Wu on 8/30/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+@interface IRDroplet : NSObject
+
+@end
---
+++ IRDroplet/IRDroplet.m
@@ -0,0 +1,23 @@
+//
+//  IRDroplet.m
+//  IRDroplet
+//
+//  Created by Evadne Wu on 8/30/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import "IRDroplet.h"
+
+@implementation IRDroplet
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
